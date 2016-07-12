d725d1ead7348bde3fed8344a53c82aae7640b67
Dan Jones <djon002@aucklanduni.ac.nz>
2016-07-12 17:40:21 +1200

    Update Analysis.sh

--- Analysis.sh
+++ Analysis.sh
@@ -5,10 +5,10 @@
 ## Analyse all fastq files using SolexaQA++ analysis, and put results in a separate directory called "analysis"
 
 # Makes the output directory
-mkdir -p analysis;
+mkdir -p $DNAWORKING/analysis;
 
 # Runs SolexaQA++ analysis and puts outputs in the analysis directory
-SolexaQA++ analysis raw/*.fastq.gz -d analysis;
+SolexaQA++ analysis $DNARAW/*.fastq.gz -d $DNAWORKING/analysis;
 
 # Opens a file window to look at the files
-nautilus analysis
+nautilus $DNAWORKING/analysis
