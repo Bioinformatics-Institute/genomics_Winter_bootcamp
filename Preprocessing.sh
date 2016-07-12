5100354e2a4340b7859ae515315bb24fedbd2124
Dan Jones <djon002@aucklanduni.ac.nz>
2016-07-12 18:07:26 +1200

    Update Preprocessing.sh

--- Preprocessing.sh
+++ Preprocessing.sh
@@ -3,18 +3,19 @@
 ## Script to preprocess files before genome assembly
 
 ## Analyse all fastq files using SolexaQA++ analysis, and put results in a separate directory called "analysis"
-
+# Change to working directory
+cd $DNAWORKING
 # Makes the output directory
-mkdir -p trimmed;
+mkdir -p $DNAWORKING/trimmed;
 
 # Runs SolexaQA++ analysis and puts outputs in the analysis directory
-SolexaQA++ dynamictrim raw/*.fastq.gz -d trimmed;
+SolexaQA++ dynamictrim $DNARAW/*.fastq.gz -d $DNAWORKING/trimmed;
 
-mkdir -p lengthsorted;
+mkdir -p $DNAWORKING/lengthsorted;
 
 for var in PE310 MP2000 MP5000; do
 
-SolexaQA++ lengthsort trimmed/${var}_L.fastq.trimmed.gz trimmed/${var}_R.fastq.trimmed.gz -d lengthsorted;
+SolexaQA++ lengthsort $DNAWORKING/trimmed/${var}_L.fastq.trimmed.gz $DNAWORKING/trimmed/${var}_R.fastq.trimmed.gz -d $DNAWORKING/lengthsorted;
 
 done
 
