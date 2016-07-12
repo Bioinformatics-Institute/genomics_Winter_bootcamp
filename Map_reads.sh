c9e1c3b57bc678f0f9e48a652ff49caa9aac9e41
Dan Jones <djon002@aucklanduni.ac.nz>
2016-07-12 20:19:48 +1200

    Update Map_reads.sh

--- Map_reads.sh
+++ Map_reads.sh
@@ -12,14 +12,14 @@ bowtie2-build $DNAWORKING/assembly/contigs.fa $DNAWORKING/index/Vp;
 # Note that we don't change the defaults of the expected minimum and maximum insert size
 mkdir -p $DNAWORKING/mapping;
 
-bowtie2 -x $DNAWORKING/index/Vp -1 $DNARAW/raw/PE310_L.fastq.gz -2 $DNARAW/raw/PE310_R.fastq.gz --minins 0 --maxins 500 -S $DNAWORKING/mapping/PE310.sam;
+bowtie2 -x $DNAWORKING/index/Vp -1 $DNARAW/PE310_L.fastq.gz -2 $DNARAW/PE310_R.fastq.gz --minins 0 --maxins 500 -S $DNAWORKING/mapping/PE310.sam;
 
 # Map just the paired-end reads separated by ~2000bp
 # Note that we change the defaults of the expected minimum and maximum insert size
-bowtie2 -x $DNAWORKING/index/Vp -1 $DNARAW/raw/MP2000_L.fastq.gz -2 $DNARAW/raw/MP2000_R.fastq.gz --minins 1700 --maxins 2700 -S $DNAWORKING/mapping/MP2000.sam;
+bowtie2 -x $DNAWORKING/index/Vp -1 $DNARAW/MP2000_L.fastq.gz -2 $DNARAW/MP2000_R.fastq.gz --minins 1700 --maxins 2700 -S $DNAWORKING/mapping/MP2000.sam;
 
 # Map just the paired-end reads separated by ~5000bp
-bowtie2 -x $DNAWORKING/index/Vp -1 $DNARAW/raw/MP5000_L.fastq.gz -2 $DNARAW/raw/MP5000_R.fastq.gz --minins 3000 --maxins 3800 -S $DNAWORKING/mapping/MP5000.sam;
+bowtie2 -x $DNAWORKING/index/Vp -1 $DNARAW/MP5000_L.fastq.gz -2 $DNARAW/MP5000_R.fastq.gz --minins 3000 --maxins 3800 -S $DNAWORKING/mapping/MP5000.sam;
 
 # Merge all the mappings together.. option commented out for now.
 
