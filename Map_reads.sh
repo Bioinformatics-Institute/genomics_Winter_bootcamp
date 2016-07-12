d87aa7befd3f7f698a2a97998bab37231682ce61
Dan Jones <djon002@aucklanduni.ac.nz>
2016-07-12 20:15:10 +1200

    Update Map_reads.sh

--- Map_reads.sh
+++ Map_reads.sh
@@ -2,23 +2,24 @@
 
 ## Script to map original reads back onto the newly assembled genome, for viewing in a browser and as a sanity check
 
+cd $DNAWORKING
 # Make a bowtie2 index for the newly assembled genome
-mkdir -p index;
+mkdir -p $DNAWORKING/index;
 
-bowtie2-build assembly/contigs.fa index/Vp;
+bowtie2-build $DNAWORKING/assembly/contigs.fa $DNAWORKING/index/Vp;
 
 # Map just the paired-end reads separated by ~310bp
 # Note that we don't change the defaults of the expected minimum and maximum insert size
-mkdir -p mapping;
+mkdir -p $DNAWORKING/mapping;
 
-bowtie2 -x index/Vp -1 raw/PE310_L.fastq.gz -2 raw/PE310_R.fastq.gz --minins 0 --maxins 500 -S mapping/PE310.sam;
+bowtie2 -x $DNAWORKING/index/Vp -1 $DNARAW/raw/PE310_L.fastq.gz -2 $DNARAW/raw/PE310_R.fastq.gz --minins 0 --maxins 500 -S $DNAWORKING/mapping/PE310.sam;
 
 # Map just the paired-end reads separated by ~2000bp
 # Note that we change the defaults of the expected minimum and maximum insert size
-bowtie2 -x index/Vp -1 raw/MP2000_L.fastq.gz -2 raw/MP2000_R.fastq.gz --minins 1700 --maxins 2700 -S mapping/MP2000.sam;
+bowtie2 -x $DNAWORKING/index/Vp -1 $DNARAW/raw/MP2000_L.fastq.gz -2 $DNARAW/raw/MP2000_R.fastq.gz --minins 1700 --maxins 2700 -S $DNAWORKING/mapping/MP2000.sam;
 
 # Map just the paired-end reads separated by ~5000bp
-bowtie2 -x index/Vp -1 raw/MP5000_L.fastq.gz -2 raw/MP5000_R.fastq.gz --minins 3000 --maxins 3800 -S mapping/MP5000.sam;
+bowtie2 -x $DNAWORKING/index/Vp -1 $DNARAW/raw/MP5000_L.fastq.gz -2 $DNARAW/raw/MP5000_R.fastq.gz --minins 3000 --maxins 3800 -S $DNAWORKING/mapping/MP5000.sam;
 
 # Merge all the mappings together.. option commented out for now.
 
@@ -28,12 +29,12 @@ bowtie2 -x index/Vp -1 raw/MP5000_L.fastq.gz -2 raw/MP5000_R.fastq.gz --minins 3
 for var in PE310 MP2000 MP5000;
 	do
 
-	samtools view -bS mapping/${var}.sam |samtools sort - mapping/${var};
-	samtools index mapping/${var}.bam
-	rm mapping/${var}.sam
+	samtools view -bS $DNAWORKING/mapping/${var}.sam |samtools sort - $DNAWORKING/mapping/${var};
+	samtools index $DNAWORKING/mapping/${var}.bam
+	rm $DNAWORKING/mapping/${var}.sam
 done;
 
-igv -g assembly/contigs.fa mapping/PE310.bam,mapping/MP2000.bam,mapping/MP5000.bam,annotation/Vp_augustus.gff3
+igv -g $DNAWORKING/assembly/contigs.fa $DNAWORKING/mapping/PE310.bam,$DNAWORKING/mapping/MP2000.bam,$DNAWORKING/mapping/MP5000.bam,$DNAWORKING/annotation/Vp_augustus.gff3
 
 
 
